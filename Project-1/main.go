004fb587f31558ed0e20217f7ac98d7ed3568f5f
simileholluwa <ajanaku.similoluwa@gmail.com>
2022-07-26 12:10:39 +0100

    Project 2. Go API with CRUD operations.

--- Project-1/main.go
+++ Project-1/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// form handler function
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -18,6 +19,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "address = %s\n", address)
 }
 
+//hello handler function
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -28,12 +30,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
+//main function
 func main() {
+
+	//refer server to static folder
 	fileServer := http.FileServer(http.Dir("./static"))
+
+	//define routes and functions
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
+	//start server
 	fmt.Printf("Starting server at port 8000\n")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Web Development With GoLang
 A repository containing a list of projects I have worked on using GoLang for web development
+* Project 1 - A simple go server
+* Project 2 - A go API with CRUD operations
